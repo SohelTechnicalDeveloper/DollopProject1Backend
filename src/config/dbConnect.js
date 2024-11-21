84ee9f3d77dd4e08fc5319efc7b7d74facafe791
SohelTechnicalDeveloper <sohelkhanp619@gmail.com>
2024-11-21 17:44:53 +0530

    commit 11/20/2024

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,10 +1,18 @@
- import mongoose from "mongoose";
- 
-  function dbConnect(URL) 
- {
-     mongoose.connect(URL)
-    console.log("dbConnect");
-    
- }
-    
- export default dbConnect
+import mongoose from "mongoose";
+
+function dbConnect(URL) {
+    mongoose
+        .connect(URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log("Database connected successfully!");
+        })
+        .catch((err) => {
+            console.error("Database connection failed:", err.message);
+            process.exit(1); // Exit the app if the DB connection fails
+        });
+}
+
+export default dbConnect;
